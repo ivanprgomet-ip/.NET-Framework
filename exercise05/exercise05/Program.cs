aa032c98ebfd039f6ea8a320fb9fbd5329729124
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-09-02 10:28:20 +0200

    exercise 5 project created. general logic for dicethrows and dictionare done

---
+++ exercise05/exercise05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //store 1000 dice throws in a dictionary<index,throw>
+            Random rnd = new Random();
+
+            int throws = 1000;
+            Dictionary<int, int> diceScores = new Dictionary<int, int>();
+
+            //store index for dice throw and throw score
+            for (int i = 0; i < throws; i++)
+            {
+                int currentThrow = rnd.Next(0, 7) + 1;
+                diceScores.Add(i, currentThrow);
+            }
+
+            //print all scores
+            foreach (KeyValuePair<int,int> kvp in diceScores)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+
+        }
+    }
+}
