f0e459ef9ec6806b2eaef71ea130f3290d53c565
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-09-02 07:27:46 +0200

    added for loop for adding key value pairs to dictionary

--- exercise04/exercise04/Program.cs
+++ exercise04/exercise04/Program.cs
@@ -10,6 +10,39 @@ namespace exercise04
     {
         static void Main(string[] args)
         {
+            //add some key value pairs to the dictionary one at a time
+            Dictionary<string, int> studentGrades = new Dictionary<string, int>();
+            studentGrades.Add("Mark", 69);
+            studentGrades.Add("Simon", 88);
+            studentGrades.Add("Anna", 97);
+            studentGrades.Add("Lisa", 47);
+            studentGrades.Add("Billy", 63);
+            studentGrades.Add("Joe", 76);
+            studentGrades.Add("Tom", 77);
+            studentGrades.Add("Anne", 93);
+            studentGrades.Add("Mary", 98);
+            studentGrades.Add("Sid", 93);
+
+            //add 3 key values using for loop al at once
+            Random rnd = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                string myStudent = "Student" + i;
+                
+                int rndGrade = rnd.Next(0, 100);
+                studentGrades.Add(myStudent, rndGrade);
+            }
+
+            //loop through dictionary and print key values
+            foreach(KeyValuePair<string,int> sg in studentGrades)
+            {
+                Console.Write($"{sg.Key}: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($" {sg.Value}p");
+                Console.ResetColor();
+            }
+
+
         }
     }
 }
