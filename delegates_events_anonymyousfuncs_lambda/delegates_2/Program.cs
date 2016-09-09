b583fba96d2df280a498a764df9ea197044a0ba5
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-09-09 17:02:29 +0200

    added delegate demo 2

---
+++ delegates_events_anonymyousfuncs_lambda/delegates_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates_2
+{
+    public delegate void HelloFunctionDelegate(string msg);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);//pass in the function (in constructor) too which you want this delegate to point to
+            del("Hello from delegate");
+        }
+        public static void Hello(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
