bd0b25ea012e2f5b664dbc92df4debed7fac6986
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-09-09 11:44:23 +0200

    anonymous method demo on a simple winform

---
+++ delegates_events_anonymyousfuncs_lambda/anonymous_methods/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace anonymous_methods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Button btnHello = new Button();
+            btnHello.Text = "Hello";
+
+            btnHello.Click += delegate
+            {
+                MessageBox.Show("Nice knowing you");
+            };
+
+            Controls.Add(btnHello);
+
+            //InitializeComponent();
+        }
+    }
+}
